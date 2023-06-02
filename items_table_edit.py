2ac11dc614fc7daee10b6bc2d828d15bbdfd021e
Marshall <marshallwknowles@gmail.com>
2023-06-02 15:36:37 -0700

    Completing items_table_edit Completed implementation of items_table_edit. Will be implementing full testing procedure at a later date.

--- items_table_edit.py
+++ items_table_edit.py
@@ -12,9 +12,9 @@ def add( database, my_cursor ):
 
     new_item_rarity = user_in.get_int("\nEnter the rarity of your item:\n", "\nInvalid input make sure your item's rarity is entered as an integer.\n")
 
-    new_item_starting_duability = user_in.get_int("\nEnter the starting durability of your item:\n", "\nInvalid input make sure your item's durability is entered as an integer.\n")
+    new_item_starting_durability = user_in.get_int("\nEnter the starting durability of your item:\n", "\nInvalid input make sure your item's durability is entered as an integer.\n")
 #adding the new item
-    my_cursor.execute("INSERT INTO items (name, rarity,starting_durability) VALUES( '" + new_item_name + " '," + str(new_item_rarity) + " ," + str(new_item_starting_duability) + ");")
+    my_cursor.execute("INSERT INTO items (name, rarity,starting_durability) VALUES( '" + new_item_name + " '," + str(new_item_rarity) + " ," + str(new_item_starting_durability) + ");")
     database.commit()
     print("Item: " + str(new_item_name) + " added!")
 
@@ -31,13 +31,13 @@ def delete( database, my_cursor ):
         #Check if the item exists:
         my_cursor.execute("SELECT * FROM items WHERE item_id = " + str(target_item_id) +";")
         target_item = my_cursor.fetchone()
-        if target_player is not None:
-            print("Player " + str(target_item_id) + " found!")
+        if target_item is not None:
+            print("Item " + str(target_item_id) + " found!")
             break
         else:
-            print("That player does not exist. Try again")
+            print("That item does not exist. Try again")
 
-    #Delete the player
+    #Delete the item
     my_cursor.execute("DELETE FROM items WHERE item_id= " + str(target_item_id) + ";")
     database.commit()
     print("Item_id: " + str(target_item_id) + " deleted.")
@@ -45,24 +45,24 @@ def delete( database, my_cursor ):
 
 
 def modify( database, my_cursor ):
-    #Prompts user for player id and player modifications and applies them to the database
+    #Prompts user for item id and item modifications and applies them to the database
 
-    #Prompt user for player_id
-    #1st level checks for player existence
+    #Prompt user for item_id
+    #1st level checks for item existence
     while(True):
         #2nd level checks for valid integer input
         item_id = user_in.get_int("\nEnter the ID number of target item: \n","That is not a valid input try inputting an integer" )
 
-        #Check if the player exists:
-        my_cursor.execute("SELECT * FROM items WHERE item_id = " + str(player_id) +";")
-        target_player = my_cursor.fetchone()
-        if target_player is not None:
-            print("Item " + str(player_id) + " found!")
+        #Check if the item exists:
+        my_cursor.execute("SELECT * FROM items WHERE item_id = " + str(item_id) +";")
+        target_item = my_cursor.fetchone()
+        if target_item is not None:
+            print("Item " + str(item_id) + " found!")
             break
         else:
             print("That item does not exist. Try again")
 
-    #Modify the player
+    #Modify the item
     while(True):
         user_input = int(input("\nWhat would you like to modify? \n 1. The item's name \n 2. The item's rarity\n 3. The item's starting durability \n"))
         match user_input:
@@ -77,12 +77,17 @@ def modify( database, my_cursor ):
 
             #Change Level
             case 2:
-                new_item_rarity = user_int.get_int("\nEnter the new item rarity:\n","That is not a valid input try inputing an integer")
+                new_item_rarity = user_in.get_int("\nEnter the new item rarity:\n","That is not a valid input try inputing an integer")
                 my_cursor.execute("UPDATE items SET rarity = "+ str(new_item_rarity) + " WHERE item_id = " + str(item_id) +";")
                 database.commit()
-                print("Item_id: " + str(item_id) + " rarity updated to: " + new_item_rarity)
+                print("Item_id: " + str(item_id) + " rarity updated to: " + str(new_item_rarity))
                 break
-
+            case 3:
+                new_item_starting_durability = user_in.get_int("\nEnter the new item starting durability:\n", "That is not a valid input try inputting an integer")
+                my_cursor.execute("UPDATE items SET starting_durability = "+ str(new_item_starting_durability) + " WHERE item_id = " + str(item_id) +";")
+                database.commit()
+                print("Item_id: " + str(item_id) + " starting durability updated to: " + str(new_item_starting_durability))
+                break
             case _:
                 print("Invalid input please try inputing an integer")
             
